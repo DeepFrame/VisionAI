fe46fc68b9c3643ff394a117df26670091eedba6
DeepFrame <anumsaeed095@gmail.com>
2025-08-06 09:16:27 +0500

    create_database&tables.sql

--- services/image_grouping/create_database&tables.sql
+++ services/image_grouping/create_database&tables.sql
@@ -1,20 +1,28 @@
-create database MetaData;
+CREATE DATABASE MetaData;
 
 USE MetaData;
 
-create table dbo.MediaFile (
-    Id INT PRIMARY KEY,
-    FilePath NVARCHAR(260) NOT NULL,
+-- 1. MediaFile Table
+CREATE TABLE dbo.MediaFile (
+    Id NVARCHAR(10) PRIMARY KEY, -- e.g., 'MF001'
+    FilePath NVARCHAR(255) NOT NULL,
     FileName NVARCHAR(100) NOT NULL,
-    MediaType NVARCHAR(10) NOT NULL,
-    ThumbnailId INT NULL 
+    MediaType VARCHAR(10) CHECK (MediaType IN ('image', 'video')) NOT NULL
 );
 
-create table dbo.MediaItems (
-    Id INT PRIMARY KEY,
-    MediaFileId INT NOT NULL,
+-- 2. ThumbnailStorage Table
+CREATE TABLE dbo.ThumbnailStorage (
+    Id NVARCHAR(10) PRIMARY KEY, -- e.g., 'TS001'
+    MediaFileId NVARCHAR(10) FOREIGN KEY REFERENCES dbo.MediaFile(Id),
+    FileName NVARCHAR(100) NOT NULL,
+    ThumbnailPath NVARCHAR(255) NOT NULL,
+    CreatedOn DATETIME NOT NULL DEFAULT GETDATE()
+);
+-- 3. MediaItems Table
+CREATE TABLE dbo.MediaItems (
+    Id NVARCHAR(10) PRIMARY KEY, -- e.g., 'MI001'
+    MediaFileId NVARCHAR(10) FOREIGN KEY REFERENCES dbo.MediaFile(Id),
     FileName NVARCHAR(100) NOT NULL,
     IsFacesExtracted BIT NOT NULL DEFAULT 0,
-    FacesExtractedOn DATETIME NULL,
-    FOREIGN KEY (MediaFileId) REFERENCES dbo.MediaFile(Id)
+    FacesExtractedOn DATETIME NULL
 );
