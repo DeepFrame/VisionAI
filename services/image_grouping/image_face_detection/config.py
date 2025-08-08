a47b974124b2bd36c79d25e1d7f5c663cba3e494
DeepFrame <anumsaeed095@gmail.com>
2025-08-08 10:21:40 +0500

    config.py

--- services/image_grouping/image_face_detection/config.py
+++ services/image_grouping/image_face_detection/config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SQL_CONNECTION_STRING = os.getenv("SQL_CONNECTION_STRING")
+
+THUMBNAIL_SAVE_PATH = os.getenv("THUMBNAIL_SAVE_PATH", "./Thumbnails")
+
+os.makedirs(THUMBNAIL_SAVE_PATH, exist_ok=True)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
