10e526a75598b2d72fe4c1fa69525d3d82465ecc
DeepFrame <anumsaeed095@gmail.com>
2025-09-04 12:40:31 +0500

    config.py

--- services/image_grouping/config.py
+++ services/image_grouping/config.py
@@ -6,6 +6,9 @@ load_dotenv()
 SQL_CONNECTION_STRING = os.getenv("SQL_CONNECTION_STRING")
 
 THUMBNAIL_SAVE_PATH = os.getenv("THUMBNAIL_SAVE_PATH", "./Thumbnails")
+SYSTEM_THUMBNAILS_PATH = os.getenv("SYSTEM_STORAGE")
+OLD_PREFIX = os.getenv("DB_PREFIX")
+NEW_PREFIX = os.getenv("CONTAINER_IMAGES_ROOT")
 
 os.makedirs(THUMBNAIL_SAVE_PATH, exist_ok=True)
 
